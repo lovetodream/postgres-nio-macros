ecec11c88c271080d80d69944c6b6f384a735f62
Timo <38291523+lovetodream@users.noreply.github.com>
2024-09-28 16:25:58 +0100

    actually test the thing

--- Sources/PreparedStatementsPostgresNIOClient/main.swift
+++ Sources/PreparedStatementsPostgresNIOClient/main.swift
@@ -2,39 +2,31 @@ import PreparedStatementsPostgresNIO
 import PostgresNIO
 import Foundation
 
-@Statement("SELECT \("id", UUID.self), \("name", String.self), \("age", Int.self) FROM users WHERE \(bind: "age", Int.self) > age")
-struct MyPreparedStatement {}
+let logger = Logger(label: "test")
 
-struct MyPreparedStatementOldWay: PostgresPreparedStatement {
-    func decodeRow(_ row: PostgresRow) throws -> Row {
-        let (v1, v2, v3) = try row.decode((UUID, String, Int).self)
-        return Row(id: v1, name: v2, age: v3)
-    }
-    
-    struct Row {
-        let id: UUID
-        let name: String
-        let age: Int
-    }
+let connection = try await PostgresConnection.connect(
+    configuration: .init(
+        host: "127.0.0.1",
+        username: "timozacherl",
+        password: nil,
+        database: "postgres",
+        tls: .disable
+    ), id: 1, logger: logger
+)
 
-    static let sql: String = ""
+try await connection.query("CREATE TABLE IF NOT EXISTS users (id UUID UNIQUE, name TEXT, age INT)", logger: logger)
 
-    let age: Int
+@Statement("INSERT INTO users (id, name, age) VALUES (\(bind: "id", UUID.self), \(bind: "name", String.self), \(bind: "age", Int.self))")
+struct SeedUser {}
 
-    func makeBindings() throws -> PostgresBindings {
-        var bindings = PostgresBindings(capacity: 1)
-        bindings.append(age)
-        return bindings
-    }
+@Statement("SELECT \("id", UUID.self), \("name", String.self), \("age", Int.self) FROM users WHERE \(bind: "age", Int.self) < age")
+struct GetForAgeRestriction {}
+
+_ = try? await connection.execute(
+    SeedUser(id: UUID(uuidString: "A0A2AE7F-C398-4468-A276-6402639806DF")!, name: "Timo", age: 24),
+    logger: logger
+)
+
+for try await row in try await connection.execute(GetForAgeRestriction(age: 18), logger: logger) {
+    print(row)
 }
-
-func myThing() {
-
-}
-
-@Statement("SELECT *")
-struct MyOtherPreparedStatement {}
-
-@available(macOS 14.0, *)
-@Observable
-final class MyObservable {}
