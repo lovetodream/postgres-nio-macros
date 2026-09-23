87068eb87719f5c1552f9462a45fe16a5a66c05c
Timo <38291523+lovetodream@users.noreply.github.com>
2026-09-23 21:29:25 +0200

    Allow using Swift Syntax for Swift 6.4 - #7 (#7)
    Signed-off-by: Timo <38291523+lovetodream@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "602.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", "602.0.0-latest"..."604.0.0-latest"),
         .package(url: "https://github.com/vapor/postgres-nio.git", from: "1.0.0"),
     ],
     targets: [
